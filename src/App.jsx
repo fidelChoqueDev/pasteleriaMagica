882f0251475c771a6150d1df1043b6571291b1c5
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2025-08-31 20:12:20 +0200

    feat:: create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
   return (
     <>
-      <div className='min-h-dvh flex flex-col items-center justify-center bg-pink-200 text-pink-900'>
+      <div className='min-h-dvh bg-gradient-to-br from-pink-100 to-pink-300 text-pink-900'>
+        <Header />
         <h1>Pasteleria MAgica</h1>
       </div>
     </>
