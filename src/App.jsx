312c5c987629845a47a730b93bf8cc0ebaa70410
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2025-09-02 00:33:04 +0200

    feat: install lucida react

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className='min-h-dvh bg-gradient-to-br from-pink-100 to-pink-300 text-pink-900'>
         <Header />
-        <h1>Pasteleria MAgica</h1>
+        <h1>Pasteleria Magica</h1>
       </div>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import {Cake} 
 
 export const Header = () => {
   return (
     <div className='flex items-center justify-between p-6'>
         <div className="flex items-center gap-2">
+            <Cake className="w-8 h-8 text-pink-600" />
             Header
         </div>
         <nav className="hidden md:flex gap-4 text-pink-700 font-medium">
