90ded25cddbdcc3bc745c1c2bda47dbce64dc677
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2025-09-03 00:04:04 +0200

    Revert "feat: install lucida react"
    This reverts commit 312c5c987629845a47a730b93bf8cc0ebaa70410.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-import {Cake} 
+import React from 'react'
 
 export const Header = () => {
   return (
     <div className='flex items-center justify-between p-6'>
         <div className="flex items-center gap-2">
-            <Cake className="w-8 h-8 text-pink-600" />
             Header
         </div>
         <nav className="hidden md:flex gap-4 text-pink-700 font-medium">
