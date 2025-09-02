b8642f7db27bdaf25086d306350a81d6d00e2d02
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2025-09-03 00:52:56 +0200

    feat:modify component Header in div logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { GiCupcake } from 'react-icons/gi';
 
 export const Header = () => {
   return (
     <div className='flex items-center justify-between p-6'>
         <div className="flex items-center gap-2">
-            Header
+            < GiCupcake className='w-8 h-8 text-pink-600'/>
+            <h1 className='text-2xl font-bold'>Pasteleria Magica</h1>
         </div>
         <nav className="hidden md:flex gap-4 text-pink-700 font-medium">
             Menu
