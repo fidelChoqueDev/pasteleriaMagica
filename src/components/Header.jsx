3963f3dc8529b41b4d5a0536a0dba5818c1629c1
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2025-09-03 20:43:10 +0200

    feat:add menu navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,11 @@ export const Header = () => {
             < GiCupcake className='w-8 h-8 text-pink-600'/>
             <h1 className='text-2xl font-bold'>Pasteleria Magica</h1>
         </div>
-        <nav className="hidden md:flex gap-4 text-pink-700 font-medium">
-            Menu
+        <nav className="bg-amber-200 hidden md:flex gap-4 text-pink-700 font-medium">
+          <a href="#inicio" className='hover:underline'>Inicio</a>
+          <a href="#productos" className='hover:underline'>Productos</a>
+          <a href="#galeria" className='hover:underline'>Galeria</a>
+          <a href="#contacto" className="hover:underline">Contacto</a>
         </nav>
     </div>
   )
