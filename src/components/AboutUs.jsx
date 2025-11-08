eed179ae7fb5ca94e8617956a0bcc216dae98344
fidelChoqueDev <devcrash03@gmail.com>
2025-11-08 21:15:34 +0100

    feat: add component About

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <section id='about' className='py-16 px-6 bg-white text-center'>
+        AboutUs
+    </section>
+  )
+}
+
+export default AboutUs
